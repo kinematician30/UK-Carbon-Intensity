ae5070d5bdb0e933226f6fd99da2cf19dabb60a0
kinematician30 <ojoayookuntade@gmail.com>
2025-03-26 20:57:00 +0100

    updated the code for orchestration

--- etl.py
+++ etl.py
@@ -3,17 +3,24 @@ import pandas as pd
 import psycopg2  # database connection and operation
 import requests  # for api interaction
 import yaml
+import logging
+
+# logging configuration
+logging.basicConfig(filename='logs/script.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
-def extract_data(URL, start_date, end_date) -> list:
+def extract_data(URL) -> list:
     headers = {
         'Accept': 'application/json',
         "User-Agent": "Mozilla/5.0"
     }
-    response = requests.get(f"{URL}/{str(start_date)}/{str(end_date)}",
+    response = requests.get(URL,
                             params={},
                             headers=headers)
-    print(f"Response Code: {response.status_code}")
+    logger.info(f"Response Code: {response.status_code}")
+    # print(f"Response Code: {response.status_code}")
     data = response.json()["data"]
     return data
 
@@ -24,7 +31,7 @@ def transform_data(data) -> list:
         time = datetime.strptime(entry['from'], '%Y-%m-%dT%H:%MZ')
         date_rec = time.strftime('%Y-%m-%d')
         time_rec = time.strftime('%H:%M')
-        day = time.strftime('%A') # day in word like Monday....
+        day = time.strftime('%A')  # day in word like Monday....
         month = time.strftime('%B')
         for region in entry['regions']:
             dnoregion = region['dnoregion']
@@ -47,8 +54,8 @@ def transform_data(data) -> list:
                 'intensity_index': intensity_index,
                 **generation_mix_data
             })
+    logger.info(f"Transformed {len(TRANSFORMED_data)} record of data, Loading now...")
     return TRANSFORMED_data
-    pass
 
 
 def connectDB() -> tuple:
@@ -61,22 +68,28 @@ def connectDB() -> tuple:
         password = config.get('password')
         port = config.get('port')
 
-        conn = psycopg2.connect(
-            dbname=db,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-        )
-        cur = conn.cursor()
-        print('Connected succesfully!')
-    return conn, cur
+        try:
+            conn = psycopg2.connect(
+                dbname=db,
+                user=user,
+                password=password,
+                host=host,
+                port=port
+            )
+            cur = conn.cursor()
+
+            logger.info('Database Connected Successfully!!')
+            print('Connected successfully!')
+            return conn, cur
+        except ConnectionError as e:
+            logger.error("Could not connect to database {}".format(e))
+            print("Bad Connection!")
 
 
 def load_data_db(data, conn, cur) -> None:
     data_count = 0
     for data_point in data:
-        # Extract values from the tranformed data
+        # Extract values from the transformed data
         date_rec = data_point['date']
         time_rec = data_point['from']
         day_recorded = data_point['day_recorded']
@@ -95,7 +108,6 @@ def load_data_db(data, conn, cur) -> None:
         solar = data_point.get('solar')
         wind = data_point.get('wind')
 
-
         # SQL query to insert data
         insert_query = """
             INSERT INTO carbon_intensity ("date", "from", day_recorded, month_recorded, 
@@ -109,34 +121,40 @@ def load_data_db(data, conn, cur) -> None:
                                    dnoregion, regionid, intensity_forecast,
                                    intensity_index, biomass, coal, imports, gas,
                                    nuclear, other, hydro, solar, wind))
+        logger.info("Inserted Successfully")
 
         # Count the number of records
         data_count += 1
 
     # Commit the changes to the database
     conn.commit()
+    logger.info(f"{data_count} records inserted successfully!")
     print(f"{data_count} records inserted successfully!")
 
 
-def load_data_csv(data, fname="carbon_intensity_data") -> None:
+def load_data_csv(data, date, fname="carbon_intensity_data",) -> None:
     df = pd.DataFrame(data)
     re_no = df.shape[0]
-    df.to_csv(f"{fname}.csv", index=True, index_label="ID")
+    df.to_csv(f"data_csv\\{fname}-{date}.csv", index=True, index_label="ID")
     print(f"{re_no} records successfully saved to csv.")
 
 
 if __name__ == "__main__":
-    BASE_URL = "https://api.carbonintensity.org.uk/regional/intensity"
-    start = date(2024, 1, 1)
-    end = date(2024, 1, 22)
+    start = date(2023, 12, 31)
+    BASE_URL = f"https://api.carbonintensity.org.uk/regional/intensity/{start}/pt24h"
 
+    logger.info("Commenced Data Extraction!")
     print("Commenced Data Extraction!")
-    data = extract_data(URL=BASE_URL, start_date=start, end_date=end)
+    data = extract_data(URL=BASE_URL)
+    logger.info("Ended Data Extraction, Commencing Transformation!")
     print("Ended Data Extraction, Commencing Transformation!")
     transformed_data = transform_data(data=data)
+    logger.info("Connecting to DB....")
     print("Connecting to DB....")
     conn, curr = connectDB()
+    logger.info('Loading Data to Database...')
     print('Loading Data to Database...')
     load_data_db(data=transformed_data, conn=conn, cur=curr)
+    logger.info("saving data to csv...")
     print("saving data to csv...")
-    load_data_csv(transformed_data)
+    load_data_csv(transformed_data, date=start)
